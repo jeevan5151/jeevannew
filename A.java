d56be569dfd0b3b13fff7bbdff5340894c8f22ac
jeevan5151 <151503790+jeevan5151@users.noreply.github.com>
2023-11-28 09:36:12 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class A {
+	
+	private int x=10;
+	int y=10;
+	
+	protected int z=10;
+	
+	public int p=100;
+	
+	public void method1A() {
+		
+		System.out.println("value of x = " +x);
+		
+	}
+
+}
